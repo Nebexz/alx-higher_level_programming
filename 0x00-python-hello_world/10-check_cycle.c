bfdda4046cdc1237aa29410d94f4c877c9a07d48
Nebexz <nebiyudaniel391@gmail.com>
2022-08-22 10:56:21 -0700

    temam

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr;
+	listint_t *prev;
+
+	ptr = list;
+	prev = list;
+	while (list && ptr && ptr->next)
+	{
+		list = list->next;
+		ptr = ptr->next->next;
+
+		if (list == ptr)
+		{
+			list = prev;
+			prev =  ptr;
+			while (1)
+			{
+				ptr = prev;
+				while (ptr->next != list && ptr->next != prev)
+				{
+					ptr = ptr->next;
+				}
+				if (ptr->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+This is a read me file for Hello world project in pytho programming
